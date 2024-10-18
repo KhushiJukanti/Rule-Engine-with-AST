3546d2f6090f44ac0ad172cbaf3c56a759c12bb6
IK <khushijukanti33@gmail.com>
2024-10-18 20:25:48 +0530

    add mongoose package and connect mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose')
 require('dotenv').config();
 
-
+const ruleRoutes = require('./routes/rules');
 
 const app = express();
 app.use(express.json())
 app.use(cors())
 
-
-app.get('/', (req,res)=>{
-    res.send('Hello Khushi')
+mongoose.connect(process.env.Mongo_uri).then(()=>{
+    console.log('mongodb connected')
+}).catch((err)=>{
+    console.log(err)
 })
 
-app.listen(7000, ()=>{
-    console.log('Sever is running')
-})
+app.use('/rules', ruleRoutes);
+
+const server = app.listen(7000, () => {
+    console.log(`Server is running`);
+});
+
+// Handle server errors
+server.on('error', (err) => {
+    if (err.code === 'EADDRINUSE') {
+        console.error(`Port 7000 is already in use. Please use a different port.`);
+    } else {
+        console.error(`Server error: ${err}`);
+    }
+});
